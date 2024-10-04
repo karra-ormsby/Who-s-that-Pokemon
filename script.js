a1fb515b91278baad5b5d5b8db82ea0b81b447f1
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2024-10-04 13:52:06 +1000

    added the hangman figures to test that out. Created a function fot game win. Function has bug atm that still takes user input after the game has been won and still calls checkWord function

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
+const mainElement = document.querySelector("main");
+const hangman = document.querySelector(".hangman");
 const guessingField = document.querySelector(".guessing-field");
-const blankArray = [];
-const chosenWordArray = [];
-//TODO: this needs to be in the function that starts the game
-let incorrectGuess = 0;
+const guess = document.querySelector(".guess");
+const topRail = document.querySelector(".top-rail");
+const noose = document.querySelector(".noose");
+const pole = document.querySelector(".pole");
+const floor = document.querySelector(".floor");
+const head = document.querySelector(".head");
+const body = document.querySelector(".body");
+const armLeft = document.querySelector(".arm-left");
+const armRight = document.querySelector(".arm-right");
+const legLeft = document.querySelector(".leg-left");
+const legRight = document.querySelector(".leg-right");
+
+
+let blankArray = [];
 let blankWord;
 let myWord;
+let incorrectGuess;
 
 const gen1 = ["bulbasaur", "ivysaur", "venusaur", "charmander", "charmeleon", "charizard", "squirtle", "wartortle", "blastoise", "caterpie", "metapod", "butterfree", "weedle", "kakuna", "beedrill", "pidgey", "pidgeotto", "pidgeot", "rattata", "raticate", "spearow", "fearow", "ekans", "arbok", "pikachu", "raichu", "sandshrew", "sandslash", "nidoran", "nidorina", "nidoqueen", "nidorino", "nidoking", "clefairy", "clefable", "vulpix", "ninetales", "jigglypuff", "wigglytuff", "zubat", "golbat", "oddish", "gloom", "vileplume", "paras", "parasect", "venonat", "venomoth", "diglett", "dugtrio", "meowth", "persian", "psyduck", "golduck", "mankey", "primeape", "growlithe", "arcanine", "poliwag", "poliwhirl", "poliwrath", "abra", "kadabra", "alakazam", "machop", "machoke", "machamp", "bellsprout", "weepinbell", "victreebel", "tentacool", "tentacruel", "geodude", "graveler", "golem", "ponyta", "rapidash", "slowpoke", "slowbrow", "magnemite", "magneton", "farfetchd", "doduo", "dodrio", "seel", "dewgong", "grimer", "muk", "shellder", "cloyster", "gastly", "haunter", "gengar", "onix", "drowzee", "hypno", "krabby", "kingler", "voltorb", "electrode", "exeggcute", "exegutor", "cubone", "marowak", "hitmonlee", "hitonchan", "lickitung", "koffing", "weezing", "rhyhorn", "rhydon", "chansey", "tangela", "kangaskhan", "horsea", "seadra", "goldeen", "seaking", "staryu", "starmie", "mrmime", "scyther", "jynx", "electabuzz", "magmar", "pinsir", "tauros", "magikarp", "gyarados", "lapras", "ditto", "eevee", "vaporeon", "jolteon", "flareon", "porygon", "omanyte", "omastar", "kabuto", "kabutops", "aerodactyl", "snorlax", "articuno", "zapdos", "moltres", "dratini", "dragonair", "dragonite", "mewtwo", "mew"];
 
 //funtion that is called when the page loads to 'set up' the game
 function init() {
+    incorrectGuess = 0;
+    blankArray = [];
     chooseWord();
     renderBlanks();
 };
@@ -29,11 +44,12 @@ function renderBlanks() {
     };
 
     blankWord = blankArray.join(" ");
-    guessingField.textContent = blankWord;
+    guess.textContent = blankWord;
 }
 
 //function that checks the letter guessed by the uer against the selected word
 //if the correct letter is gussed it will replace the '_' at it's loaction and be rendered to the document
+//the function then checks if the user has guessed all letters in the word
 //if the letter gusssed is incorrect it runs the wrongGuess function
 function checkWord() {
     const letter = event.key;
@@ -49,6 +65,11 @@ function checkWord() {
     if (!correctGuess) {
         incorrectGuess ++;
         wrongGuess();
+    } else {
+        const blankword = blankArray.join("");
+        if (blankword === myWord) {
+            gameWin();
+        }
     }
 };
 
@@ -58,50 +79,78 @@ function wrongGuess() {
         switch (incorrectGuess) {
             case 1:
                 console.log("head");
+                head.classList.remove("hidden");
                 break;
             case 2:
                 console.log("body");
+                body.classList.remove("hidden");
                 break;
             case 3:
                 console.log("left arm");
+                armLeft.classList.remove("hidden");
                 break;
             case 4:
                 console.log("right atm");
+                armRight.classList.remove("hidden");
                 break;
             case 5:
                 console.log("left leg");
+                legLeft.classList.remove("hidden");
                 break;
             case 6:
                 console.log("right lef");
+                legRight.classList.remove("hidden");
                 break;
             case 7:
                 console.log("noose");
+                noose.classList.remove("hidden");
                 break;
             case 8:
                 console.log("top pole");
+                topRail.classList.remove("hidden");
                 break;
             case 9:
                 console.log("pole");
+                pole.classList.remove("hidden");
                 break;
             case 10:
                 console.log("floor");
+                floor.classList.remove("hidden");
                 gameOver()
                 break;
         }
 };
 
-//function that once max inccorect guesses in reached, it renders a new page an offers to let the uer play again
+//function that when the correct answer is reached, it renders a new page, tells the user the answeran offers to let the uer play again
+//TODO: game still take user. Will continue to run checkWord and either run gameWin or increment incorrectGuess and run gameOver when incorrectGuess reaches 10
+function gameWin() {
+    guessingField.innerHTML = '';
+    hangman.innerHTML = '';
+    const winner = document.createElement("p");
+    winner.textContent = "You're a winner!";
+    mainElement.appendChild(winner);
+    renderReset();
+};
+
+//function that once max inccorect guesses is reached, it renders a new page, tells the user the answeran offers to let the uer play again
 function gameOver() {
-    console.log(`the word was ${myWord}. Would you like to play again?`);
     guessingField.innerHTML = "";
+    hangman.innerHTML = '';
+    const answer = document.createElement("p");
+    answer.textContent = `The correct answer was ${myWord}. Bettermewo luck next time.`;
+    mainElement.appendChild(answer);
+    renderReset();
+};
+
+//function that creates a reset button for the game and adds and event listerner to it
+function renderReset() {
     const resetBtn = document.createElement("div");
     resetBtn.setAttribute("class", "reset");
     resetBtn.textContent = "Play Again?";
-    guessingField.appendChild(resetBtn);
+    mainElement.appendChild(resetBtn);
     const playAgain = document.querySelector(".reset");
     playAgain.addEventListener("click", init);
 };
-
 
 init(); 
 
