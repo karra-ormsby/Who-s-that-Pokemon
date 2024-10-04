5000294b2fdd3a1b739bcb5edcca7bb8534fb7c3
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2024-10-04 17:34:27 +1000

    created a function to render the hangman with js instead of having it hardcoded in the html

--- script.js
+++ script.js
@@ -2,43 +2,35 @@ const mainElement = document.querySelector("main");
 const hangman = document.querySelector(".hangman");
 const guessingField = document.querySelector(".guessing-field");
 const guess = document.querySelector(".guess");
-const topRail = document.querySelector(".top-rail");
-const noose = document.querySelector(".noose");
-const pole = document.querySelector(".pole");
-const floor = document.querySelector(".floor");
-const head = document.querySelector(".head");
-const body = document.querySelector(".body");
-const armLeft = document.querySelector(".arm-left");
-const armRight = document.querySelector(".arm-right");
-const legLeft = document.querySelector(".leg-left");
-const legRight = document.querySelector(".leg-right");
-
 
 let blankArray = [];
 let blankWord;
 let myWord;
 let incorrectGuess;
 let gameCompeted;
+let hangmanElements;
 
 const gen1 = ["bulbasaur", "ivysaur", "venusaur", "charmander", "charmeleon", "charizard", "squirtle", "wartortle", "blastoise", "caterpie", "metapod", "butterfree", "weedle", "kakuna", "beedrill", "pidgey", "pidgeotto", "pidgeot", "rattata", "raticate", "spearow", "fearow", "ekans", "arbok", "pikachu", "raichu", "sandshrew", "sandslash", "nidoran", "nidorina", "nidoqueen", "nidorino", "nidoking", "clefairy", "clefable", "vulpix", "ninetales", "jigglypuff", "wigglytuff", "zubat", "golbat", "oddish", "gloom", "vileplume", "paras", "parasect", "venonat", "venomoth", "diglett", "dugtrio", "meowth", "persian", "psyduck", "golduck", "mankey", "primeape", "growlithe", "arcanine", "poliwag", "poliwhirl", "poliwrath", "abra", "kadabra", "alakazam", "machop", "machoke", "machamp", "bellsprout", "weepinbell", "victreebel", "tentacool", "tentacruel", "geodude", "graveler", "golem", "ponyta", "rapidash", "slowpoke", "slowbrow", "magnemite", "magneton", "farfetchd", "doduo", "dodrio", "seel", "dewgong", "grimer", "muk", "shellder", "cloyster", "gastly", "haunter", "gengar", "onix", "drowzee", "hypno", "krabby", "kingler", "voltorb", "electrode", "exeggcute", "exegutor", "cubone", "marowak", "hitmonlee", "hitonchan", "lickitung", "koffing", "weezing", "rhyhorn", "rhydon", "chansey", "tangela", "kangaskhan", "horsea", "seadra", "goldeen", "seaking", "staryu", "starmie", "mrmime", "scyther", "jynx", "electabuzz", "magmar", "pinsir", "tauros", "magikarp", "gyarados", "lapras", "ditto", "eevee", "vaporeon", "jolteon", "flareon", "porygon", "omanyte", "omastar", "kabuto", "kabutops", "aerodactyl", "snorlax", "articuno", "zapdos", "moltres", "dratini", "dragonair", "dragonite", "mewtwo", "mew"];
 
 //funtion that is called when the page loads to 'set up' the game
 function init() {
+    gameEnd.innerHTML = "";
     incorrectGuess = 0;
     blankArray = [];
     gameCompeted = false;
     chooseWord();
     renderBlanks();
+    hangmanElements = createHangman();
 };
 
-//function to choose a random ny=umber from 1 to lengnth of array of possible words
+//chooses a random ny=umber from 1 to lengnth of array of possible words
 //TODO: change gen1 to choosenGen (where choosenGen will be equal to the array that the user picks)
 function chooseWord () {
     myWord = gen1[Math.floor(Math.random() * gen1.length)];
     console.log(myWord);
 };
 
-//function to turn myWord into an array of blanks that will be displayed as the 'word' to the user
+//turn myWord into an array of blanks that will be displayed as the 'word' to the user
 //then display the array as a string to the user
 function renderBlanks() {
     for(let i = 0; i < myWord.length; i++) {
@@ -49,7 +41,7 @@ function renderBlanks() {
     guessingField.textContent = blankWord;
 }
 
-//function that checks the letter guessed by the uer against the selected word
+//checks the letter guessed by the uer against the selected word
 //if the correct letter is gussed it will replace the '_' at it's loaction and be rendered to the document
 //the function then checks if the user has guessed all letters in the word
 //if the letter gusssed is incorrect it runs the wrongGuess function
@@ -82,73 +74,62 @@ function checkWord() {
 function wrongGuess() {
         switch (incorrectGuess) {
             case 1:
-                console.log("head");
-                head.classList.remove("hidden");
+                hangmanElements.head.classList.remove("hidden"); 
                 break;
             case 2:
-                console.log("body");
-                body.classList.remove("hidden");
+                hangmanElements.body.classList.remove("hidden");
                 break;
             case 3:
-                console.log("left arm");
-                armLeft.classList.remove("hidden");
+                hangmanElements.armLeft.classList.remove("hidden");
                 break;
             case 4:
-                console.log("right atm");
-                armRight.classList.remove("hidden");
+                hangmanElements.armRight.classList.remove("hidden");
                 break;
             case 5:
-                console.log("left leg");
-                legLeft.classList.remove("hidden");
+                hangmanElements.legLeft.classList.remove("hidden");
                 break;
             case 6:
-                console.log("right lef");
-                legRight.classList.remove("hidden");
+                hangmanElements.legRight.classList.remove("hidden");
                 break;
             case 7:
-                console.log("noose");
-                noose.classList.remove("hidden");
+                hangmanElements.noose.classList.remove("hidden");
                 break;
             case 8:
-                console.log("top pole");
-                topRail.classList.remove("hidden");
+                hangmanElements.topRail.classList.remove("hidden");
                 break;
             case 9:
-                console.log("pole");
-                pole.classList.remove("hidden");
+                hangmanElements.pole.classList.remove("hidden");
                 break;
             case 10:
-                console.log("floor");
-                floor.classList.remove("hidden");
+                hangmanElements.floor.classList.remove("hidden");
                 gameOver()
                 break;
         }
 };
 
-//function that when the correct answer is reached, it renders a new page, tells the user the answeran offers to let the uer play again
-//TODO: game still take user. Will continue to run checkWord and either run gameWin or increment incorrectGuess and run gameOver when incorrectGuess reaches 10
+const gameEnd = document.createElement("p");
+
+//when the correct answer is reached, it renders a new page, tells the user the answeran offers to let the uer play again
 function gameWin() {
     gameCompeted = true;
-    guessingField.innerHTML = '';
-    hangman.innerHTML = '';
-    const winner = document.createElement("p");
-    winner.textContent = "You're a winner!";
-    guessingField.appendChild(winner);
+    guessingField.innerHTML = "";
+    hangmanElements.figureField = "";
+    gameEnd.textContent = "You're a winner!";
+    mainElement.appendChild(gameEnd);
     renderReset();
 };
 
-//function that once max inccorect guesses is reached, it renders a new page, tells the user the answeran offers to let the uer play again
+//once max inccorect guesses is reached, it renders a new page, tells the user the answeran offers to let the uer play again
 function gameOver() {
     gameCompeted = true;
     guessingField.innerHTML = "";
-    hangman.innerHTML = '';
-    const answer = document.createElement("p");
-    answer.textContent = `The correct answer was ${myWord}. Better luck next time.`;
-    guessingField.appendChild(answer);
+    hangmanElements.figureField.innerHTML = "";
+    gameEnd.textContent = `The correct answer was ${myWord}. Better luck next time.`;
+    mainElement.appendChild(gameEnd);
     renderReset();
 };
 
-//function that creates a reset button for the game and adds and event listerner to it
+//creates a reset button for the game and adds and event listerner to it
 function renderReset() {
     const resetBtn = document.createElement("div");
     resetBtn.setAttribute("class", "reset");
@@ -156,6 +137,60 @@ function renderReset() {
     guessingField.appendChild(resetBtn);
     const playAgain = document.querySelector(".reset");
     playAgain.addEventListener("click", init);
+};
+
+//creates the hangman and gives it the class hidden so that the parts of the hangman are hidden until an incorrect guess happens
+function createHangman() {
+    const figureField = document.createElement("div");
+    figureField.setAttribute("class", "hangman");
+    const topRail = document.createElement("div");
+    topRail.setAttribute("class", "top-rail hidden hidden");
+    const noose = document.createElement("div");
+    noose.setAttribute("class", "noose hidden");
+    const pole = document.createElement("div");
+    pole.setAttribute("class", "pole hidden");
+    const floor = document.createElement("div");
+    floor.setAttribute("class", "floor hidden");
+    const head = document.createElement("div");
+    head.setAttribute("class", "head hidden");
+    const body = document.createElement("div");
+    body.setAttribute("class", "body hidden");
+    const armLeft = document.createElement("div");
+    armLeft.setAttribute("class", "arm-left hidden");
+    const armRight = document.createElement("div");
+    armRight.setAttribute("class", "arm-right hidden");
+    const legLeft = document.createElement("div");
+    legLeft.setAttribute("class", "leg-left hidden");
+    const legRight = document.createElement("div");
+    legRight.setAttribute("class", "leg-right hidden");
+
+    figureField.appendChild(topRail);
+    figureField.appendChild(noose);
+    figureField.appendChild(pole);
+    figureField.appendChild(floor);
+    figureField.appendChild(head);
+    figureField.appendChild(body);
+    figureField.appendChild(armLeft);
+    figureField.appendChild(armRight);
+    figureField.appendChild(legLeft);
+    figureField.appendChild(legRight);
+
+    mainElement.appendChild(figureField);
+
+     return {
+        figureField,
+        topRail,
+        noose,
+        pole,
+        floor,
+        head,
+        body,
+        armLeft,
+        armRight,
+        legLeft,
+        legRight
+    };
+
 };
 
 init(); 
