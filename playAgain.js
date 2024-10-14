3a6191ac8daff9dd6cc1ca6944507fd337a2b64f
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2024-10-14 18:46:48 +1100

    resized and animated the pokemon sprite. Added the pokemon cry to the final page as well

--- playAgain.js
+++ playAgain.js
@@ -1,11 +1,16 @@
 const playAgainBtn = document.querySelector("#play-again");
 const myWord = localStorage.getItem("myWord");
 const pokedexNum = localStorage.getItem("pokedexNum");
+const pokemonSprite = document.querySelector("#pokemon-sprite");
+const pokemonCry = document.querySelector("#pokemon-cry");
+const audioElement = document.querySelector("audio");
 
-
-const pokemonSprite = document.createElement("img");
-document.querySelector("main").prepend(pokemonSprite);
 pokemonSprite.setAttribute("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokedexNum}.png`);
+pokemonCry.setAttribute("src", `https://veekun.com/dex/media/pokemon/cries/${pokedexNum}.ogg`);
+
+// Load and play the audio
+audioElement.load();
+audioElement.play();
 
 if (myWord && myWord.length > 0) {
     const answer = document.querySelector("#answer");
