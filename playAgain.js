d3f07ec124fc7109c7bb54c9f3ed635b9800115c
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2024-10-16 18:15:36 +1100

    using the pokeapi api for sprite and cries instead of multiple urls.

--- playAgain.js
+++ playAgain.js
@@ -3,16 +3,31 @@ const playAgainBtn = document.querySelector("#play-again");
 const myWord = localStorage.getItem("myWord");
 const pokedexNum = localStorage.getItem("pokedexNum");
 const gameWon = JSON.parse(localStorage.getItem("gameWon"));
-const pokemonSprite = document.querySelector("#pokemon-sprite");
-const pokemonCry = document.querySelector("#pokemon-cry");
-const audioElement = document.querySelector("audio");
 
-pokemonSprite.setAttribute("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokedexNum}.png`);
-pokemonCry.setAttribute("src", `https://veekun.com/dex/media/pokemon/cries/${pokedexNum}.ogg`);
-
-// Load and play the audio
-audioElement.load();
-audioElement.play();
+fetch(`https://pokeapi.co/api/v2/pokemon/${pokedexNum}/`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log("data: ", data);
+            const frontSprite = data.sprites.front_default;
+            const latestCry = data.cries.latest;
+            const legacyCry = data.cries.legacy;
+            console.log(frontSprite);
+            const pokemonSprite = document.querySelector("#pokemon-sprite");
+            pokemonSprite.setAttribute("src", frontSprite);
+            const pokemonCry = document.querySelector("#pokemon-cry");
+            pokemonCry.setAttribute("src", latestCry); 
+            const audioElement = document.querySelector("audio");
+            audioElement.load();
+            audioElement.play();
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
 
 if (gameWon) {
     message.textContent = "Congratulations you won!";
